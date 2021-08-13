caf17ea8aef400d135ae5349005e467929044ac6
JabriHassane <75222601+JabriHassane@users.noreply.github.com>
2021-08-13 15:06:07 +0100

    send emails

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -3,7 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ContactType;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPUnit\Util\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,13 +16,76 @@ class PortfolioController extends AbstractController
     /**
      * @Route("/", name="portfolio")
      */
-    public function index()
+    public function index(Request $request, \Swift_Mailer $mailer)
     {
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+
         $articles = $this->getDoctrine()->getRepository(Article::class)->findBy(
             ['isPublished' => true],
             ['publicationDate' => 'desc']
         );
 
-        return $this->render('portfolio/index.html.twig', ['articles' => $articles]);
+        if($form->isSubmitted() && $form->isValid()){
+            $contact = $form->getData();
+            $from = $contact['email'];
+            $name = $contact['name'];
+            $content = $contact['message'];
+
+            try {
+            $mail = new PHPMailer(true);
+            //$mail->isSMTP();
+            $mail->SMTPAuth=true;
+            $mail->SMTPSecure = 'ssl';
+            $mail->Host = 'smtp.gmail.com';
+            $mail->Port = '587';
+            $mail->isHTML();
+            $mail->Username = 'devlop.pev@gmail.com';
+            $mail->Password = 'sony.figuig';
+            $mail->setFrom('test@me.com');
+            $mail->Subject = 'Nouveau Contact '.$name;
+            $mail->Body = $from.'\n'.$content;
+            $mail->addAddress('devlop.pev@gmail.com');
+
+
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+
+            //cree et envoyer le message
+            /*
+             $message = (new \Swift_Message(('Nouveau Contact')))
+                ->setFrom($contact['email'])
+                ->setTo('devlop.pev@gmail.com')
+                ->setBody(
+                    $this->renderView(
+                        'emails/contact.html.twig', compact('contact')
+                    ),
+                    'text/html'
+                )
+                ;
+
+            $mailer->send($message);
+            $this->addFlash('message', 'Le message a bien été envoyer!');
+
+            */
+
+
+            return $this->render('portfolio/index.html.twig', [
+                'articles' => $articles,
+                'message' => 'Votre Message a ete envoyer avec succe! Merci',
+                'contactForm'=>$form->createView()
+            ]);
+
+        }
+
+
+
+        return $this->render('portfolio/index.html.twig', [
+            'articles' => $articles,
+            'contactForm'=>$form->createView()
+        ]);
     }
 }
